82c203a5ce67319a1353f07351c9fdd1b2ef35b5
hawkiq <osama.sw87@yahoo.com>
2022-06-23 10:13:53 +0300

    Fixed Big in username +Numbers with comma

--- src/HawkiqPSN.php
+++ src/HawkiqPSN.php
@@ -5,7 +5,7 @@
  * User: Osama
  * Date: 12/03/2022
  * @author OsaMa hawkiq
- * @version 1.0.2
+ * @version 1.0.3
  * @package hawkiq/larapsn
  */
 
@@ -31,17 +31,17 @@ class HawkiqPSN
     /*
     * createHtmlObject get dom and save it for parsing to save bandwidth
     */
-    private function createHtmlObject($psnID, $type = 1)
+    private function createHtmlObject($type = 1)
     {
         switch ($type) {
             case 1:
-                $url = 'https://psnprofiles.com/' . $psnID . '/';
+                $url = 'https://psnprofiles.com/' . $this->psnid . '/';
                 break;
             case 2:
-                $url = 'https://psnprofiles.com/' . $psnID . '/stats';
+                $url = 'https://psnprofiles.com/' . $this->psnid . '/stats';
                 break;
             default:
-                $url = 'https://psnprofiles.com/' . $psnID . '/';
+                $url = 'https://psnprofiles.com/' . $this->psnid . '/';
                 break;
         }
         $html = HtmlDomParser::file_get_html($url);
@@ -79,15 +79,15 @@ class HawkiqPSN
     }
     public function getInfos()
     {
-        $html = $this->createHtmlObject($this->psnid);
-        $username = $html->find('span[class=username]', 0)->plaintext;
+        $html = $this->createHtmlObject();
+        $username = trim($html->find('span[class=username]', 0)->plaintext);
         $about = $html->find('span[class=comment]', 0)->plaintext;
         $level = $html->find('li[class=icon-sprite level]', 0)->plaintext;
         $trophies = $this->getTrophies($html);
         $avatar = $this->getAvatar($html);
         for ($i = 0; $i < 3; $i++) {
             $element = $html->find('span[class=stat grow]', $i)->plaintext;
-            $search = array("Games Played ", "Completed Games ", "Completion ", "Unearned Trophies  ", "Trophies Per Day  ", "Average Rarity  ", "Views  ", "%");
+            $search = array(",","Games Played ", "Completed Games ", "Completion ", "Unearned Trophies  ", "Trophies Per Day  ", "Average Rarity  ", "Views  ", "%");
             $newstr = str_replace($search, '', $element);
             $stats[$i] = $newstr;
         }
@@ -97,7 +97,7 @@ class HawkiqPSN
         $hiddensrc = $html->find('div[id=hidden-trophies]', 0) != null ? $html->find('div[id=hidden-trophies]', 0)->title : '';
         if (strlen($hiddensrc) > 0) {
             $hide = explode(' ', $hiddensrc);
-            $hidden = strip_tags($hide[4]);
+            $hidden = str_replace(',','',strip_tags($hide[4]));
         }
         $info = array("username" => $username, "about" => $about, "avatar" => $avatar, "trophies" => $trophies, "level" => $level, "lastgame" => $stats[3], "played" => $stats[0], "completion" => $stats[2], "complgames" => $stats[1], "hiddentrophies" => $hidden);
         return $info;
@@ -106,9 +106,9 @@ class HawkiqPSN
     public function getRarity()
     {
         $info_r = array();
-        $html = $this->createHtmlObject($this->psnid, 2);
+        $html = $this->createHtmlObject(2);
         $rares = $html->find('ul[class=legend]', 3)->plaintext;
-        $search = array("Ultra", "Very", "Rare", "Uncommon", "Common", "(", ")", ",");
+        $search = array("Ultra", "Very", "Rare", "Uncommon", "Common", "(", ")", ",","\t");
         $rares = str_ireplace($search, '', $rares);
         $array_rare = explode(' ', $rares);
         $info_r = array("ultrarare" => $array_rare[4], "veryrare" => $array_rare[8], "rare" => $array_rare[11], "uncommon" => $array_rare[14], "common" => $array_rare[17]);
